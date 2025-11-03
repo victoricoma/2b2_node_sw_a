eb07765b3fc31676c735cb6e483c2d1f5ff4bf8b
victoricoma <victor.icoma@fatec.sp.gov.br>
2025-11-03 20:33:49 -0300

    Avaliação Participação EAD

--- accounts/aula3/index.js
+++
@@ -1,3 +0,0 @@
-const chalk = require("chalk")
-
-console.log(chalk.bgGreen.black("Node SW 2B2"))
---
+++ empregados/app/src/models/departamento.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+    const Departamento = sequelize.define('Departamento', {
+        cod_depto: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nome: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        }
+    }, {
+        tableName: 'departamento',
+        timestamps: false,
+        underscored: true
+    });
+
+    Departamento.associate = (models) => {
+        Departamento.hasMany(models.Empregado, { foreignKey: 'cod_depto', as: 'empregados' });
+    };
+    return Departamento;
+};
